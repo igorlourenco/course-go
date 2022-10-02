0bc9ce35fbc39ac4300b2f7cec798a1d0fc0972a
Igor Lourenço <igorlourenco.dev@gmail.com>
2022-10-02 08:09:09 -0300

    learn zeros

---
+++ first-steps/zeros/zeros.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b float64
+	var c bool
+	var d string
+	var e *int
+
+	fmt.Println("zero of int:", a)
+	fmt.Println("zero of float:", b)
+	fmt.Println("zero of bool:", c)
+	fmt.Println("zero of string:", d)
+	fmt.Println("zero of pointer (int):", e)
+}
